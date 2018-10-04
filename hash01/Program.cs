ae14a3aa0305717ba6d23a1397335c85a8c2219f
aitorgit <tugasmec@gmail.com>
2018-10-04 21:08:57 +0200

    first commit

--- hash01/Program.cs
+++ hash01/Program.cs
@@ -1,24 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace hash01
 {
-// Path.GetFullpath("TextFile1.txt");
+    // Path.GetFullpath("TextFile1.txt");
     class Program
     {
+
         static void Main(string[] args)
         {
-            String textIn = null;
+            string route;
+            string TextFileToHash;
+
             Console.Write("Entra text: ");
-            while (textIn==null)
-                textIn = Console.ReadLine();
+
+            route = Path.GetFullPath("TextFile1.txt");  
+            TextFileToHash = File.ReadAllText(route);
 
             // Convertim l'string a un array de bytes
-            byte[] bytesIn = Encoding.UTF8.GetBytes(TextFile1.txt);
+            byte[] bytesIn = Encoding.UTF8.GetBytes(TextFileToHash);
             // Instanciar classe per fer hash
             SHA512Managed SHA512 = new SHA512Managed();
             // Calcular hash
@@ -27,7 +29,7 @@ namespace hash01
             // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
             // cal convertir-lo a un string
 
-             String textOut = BitConverter.ToString(hashResult).Replace("-", string.Empty);
+            String textOut = BitConverter.ToString(hashResult).Replace("-", string.Empty);
             Console.WriteLine("Hash del text{0}", textIn);
             Console.WriteLine(textOut);
             Console.ReadKey();
