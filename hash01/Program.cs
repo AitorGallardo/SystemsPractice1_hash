09c207ba9753111b8564c2fdab5ca5d09c9b6dc5
aitorgit <tugasmec@gmail.com>
2018-10-05 16:28:02 +0200

    v1 Reading and hashing content of TextFile1.txt
    (Finished.)

--- hash01/Program.cs
+++ hash01/Program.cs
@@ -14,28 +14,38 @@ namespace hash01
             string route;
             string TextFileToHash;
 
-            Console.Write("Entra text: ");
+            try {
 
-            route = Path.GetFullPath("TextFile1.txt");  
-            TextFileToHash = File.ReadAllText(route);
+                route = Path.GetFullPath("ytyt");
+                TextFileToHash = File.ReadAllText(route);
 
-            // Convertim l'string a un array de bytes
-            byte[] bytesIn = Encoding.UTF8.GetBytes(TextFileToHash);
-            // Instanciar classe per fer hash
-            SHA512Managed SHA512 = new SHA512Managed();
-            // Calcular hash
-            byte[] hashResult = SHA512.ComputeHash(bytesIn);
+                // Convertim l'string a un array de bytes
+                byte[] bytesIn = Encoding.UTF8.GetBytes(TextFileToHash);
+                // Instanciar classe per fer hash
+                SHA512Managed SHA512 = new SHA512Managed();
+                // Calcular hash
+                byte[] hashResult = SHA512.ComputeHash(bytesIn);
 
-            // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
-            // cal convertir-lo a un string
+                // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
+                // cal convertir-lo a un string
 
-            String textOut = BitConverter.ToString(hashResult).Replace("-", string.Empty);
-            Console.WriteLine("Hash del text{0}", textIn);
-            Console.WriteLine(textOut);
+                String textOut = BitConverter.ToString(hashResult).Replace("-", string.Empty);
+                Console.WriteLine("Hash del text {0}", TextFileToHash);
+                Console.WriteLine(textOut);
+                
+
+                // Eliminem la classe instanciada
+                SHA512.Dispose();
+            }
+            catch (FileNotFoundException e) {
+                // FileNotFoundException
+                Console.WriteLine("{0} File ------->NOT<--------------- found", e);
+            }
+
             Console.ReadKey();
 
-            // Eliminem la classe instanciada
-            SHA512.Dispose();
+
+
         }
     }
 }
